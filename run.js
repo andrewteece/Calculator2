85156c5e0a919245655d1d1bead8c3aaa1031dde
Andrew Teece <andrewteece@gmail.com>
2024-04-06 15:30:52 -0500

    index and run js code

---
+++ run.js
@@ -0,0 +1,33 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+export function run(operation, x, y) {
+  console.log(operation);
+  console.log(x, y);
+  //write your code here
+  if (operation === 'sum') {
+    return sum(x, y);
+  }
+  if (operation === 'multiply') {
+    return multiply(x, y);
+  }
+  if (operation === 'subtract') {
+    return subtract(x, y);
+  }
+  if (operation === 'divide') {
+    return divide(x, y);
+  }
+}
